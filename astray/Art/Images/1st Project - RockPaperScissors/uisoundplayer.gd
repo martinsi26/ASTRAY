4a1f33f59bdbe5a3ef175f4734cd777dd0baa9e8
kayv1273 <vuk25@up.edu>
2024-09-09 11:59:08 -0700

    Added Assets

---
+++ astray/Art/Images/1st Project - RockPaperScissors/uisoundplayer.gd
@@ -0,0 +1,12 @@
+extends AudioStreamPlayer2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var button_sfx = $button_SFX
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
