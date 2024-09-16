40fe1b4e3186b33319ae7f1328582f6e793dcf2d
martinsi26 <martinsi26@up.edu>
2024-09-16 13:16:05 -0700

    removed print statements

--- astray/Script/Main.gd
+++ astray/Script/Main.gd
@@ -33,7 +33,6 @@ signal tree_room
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("in main")
 	# Enter Room 1
 	add_child(instance1)
 	$Cat.position = instance1.get_node("Spawn").position
