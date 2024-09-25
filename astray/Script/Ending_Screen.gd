c585f54234ee00d1f31f5e81bdf2659c7b867624
moreym26 <moreym26@up.edu>
2024-09-25 13:56:45 -0700

    EndScreen&HelpScreenFix
    I fixed the Help screen button and added a play again button to the ending screen.

--- astray/Script/Ending_Screen.gd
+++ astray/Script/Ending_Screen.gd
@@ -3,3 +3,7 @@ extends Control
 func _on_quit_button_pressed() -> void:
 	#When quit button is pressed game quits
 	get_tree().quit()
+
+
+func _on_play_again_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/StartScreen.tscn")
