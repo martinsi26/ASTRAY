063b6807c96ea9c18c9615b19646946841dd2bf6
moreym26 <moreym26@up.edu>
2024-09-28 15:17:00 -0700

    added button to end screen

--- astray/Script/Ending_Screen.gd
+++ astray/Script/Ending_Screen.gd
@@ -3,3 +3,8 @@ extends Control
 func _on_quit_button_pressed() -> void:
 	#When quit button is pressed game quits
 	get_tree().quit()
+
+
+func _on_return_to_home_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/StartScreen.tscn")
+	#Returns to home screen at the end of the game
