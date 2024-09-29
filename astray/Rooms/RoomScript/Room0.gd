04f1c659ce7e9ac65aad05750255345979b0874a
moreym26 <moreym26@up.edu>
2024-09-28 17:50:23 -0700

    Added mouse and dialogue for paper
    I added the cute mouse toy and added dialogue for the paper in room 0.

--- astray/Rooms/RoomScript/Room0.gd
+++ astray/Rooms/RoomScript/Room0.gd
@@ -1,6 +1,17 @@
 extends Node2D
 
+
+var dialogue = preload("res://Scene/Dialogue.tscn")
+
+signal start_dialogue
 signal enter_room1(current_room, to_room)
 
 func _on_to_room_1_area_entered(area: Area2D) -> void:
 	emit_signal("enter_room1", 0, 1)
+	
+func poster_dialogue():
+	dialogue = [
+		"Lost Kitten: Ash",
+		"Description: Small with black furr and green eyes."
+	]
+	emit_signal("start_dialogue", dialogue)
